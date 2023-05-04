11b00269d16d67708d9f94047ef13fccc41d787a
Ankit Mongia <ankit.mongia@algosec.com>
2023-05-04 17:28:31 +0530

    Create dictionary component

---
+++ src/components/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import emojipedia from "../emojipedia";
+import Entry from "./Entry";
+
+function createEntry(emoji){
+  return (
+    <Entry 
+    key={emoji.id} 
+    name={emoji.name} 
+    meaning={emoji.meaning} 
+    emojiIcon={emoji.emoji} 
+    />
+  );
+}
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">
+          {emojipedia.map(createEntry)}
+      </dl>
+    </div>
+  );
+}
+
+export default App;
